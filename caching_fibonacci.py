633acdec9fb9afafa6cddb56d02d6dcc1c09529a
blackkat02 <boris02374@gmail.com>
2024-07-17 19:36:29 +0300

    Added caching functionality for Fibonacci sequence calculation.

--- caching_fibonacci.py
+++ caching_fibonacci.py
@@ -2,11 +2,29 @@ from typing import Callable
 
 
 def caching_fibonacci() -> Callable[[int], int]:
+    """
+    This function creates a closure that implements a memoized version of the
+    Fibonacci sequence. Memoization stores previously calculated results in a
+    cache to avoid redundant calculations.
+
+    Returns:
+        Callable[[int], int]: A function that calculates the nth Fibonacci number
+        using memoization.
+    """
+
     print("start 'caching_fibonacci'")
     cache = {}
     print(f"cache after start: {cache}")
 
     def fibonacci(n: int) -> int:
+        """
+        Calculates the nth Fibonacci number using memoization.
+        Args:
+            n (int): The index of the Fibonacci number to calculate.
+                    Must be non-negative.
+        Returns:
+            int: The nth Fibonacci number.
+        """
         if n < 0:
             return 0
         elif n <= 1:
